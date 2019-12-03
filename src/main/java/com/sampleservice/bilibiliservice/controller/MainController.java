5d124f7b596940e622fb5b40c5163cce20af28d9
SeptemberHX <september_hx@outlook.com>
2019-12-03 16:56:56 +0800

    f

--- src/main/java/com/sampleservice/bilibiliservice/controller/MainController.java
+++ src/main/java/com/sampleservice/bilibiliservice/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.sampleservice.bilibiliservice.controller;
 
+import com.sampleservice.bilibiliservice.utils.MBaseUtils;
 import com.septemberhx.common.base.MResponse;
 import com.septemberhx.mclient.annotation.MApiFunction;
 import com.septemberhx.mclient.annotation.MRestApiType;
@@ -20,6 +21,10 @@ public class MainController extends MObject {
     public MResponse streamFunction(@RequestBody MResponse requestData) {
         MResponse result = new MResponse();
         result.set("msg", "/stream");
+
+        if (requestData.get("interval") != null) {
+            MBaseUtils.generateStringInKBSize(20, result, (int) requestData.get("interval"));
+        }
         return result;
     }
 }
